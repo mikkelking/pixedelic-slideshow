9923648cc21df2abaa1bddd5784c3a1fb560adb1
Mike king <mikkelking@hotmail.com>
2015-09-16 20:21:00 +1000

    Allow data-img as well as data-src to get around an Angular security issue

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mikkelking:pixedelic-slideshow',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Meteor package to wrap pixedelic/Camera',
   // URL to the Git repository containing the source code for this package.
